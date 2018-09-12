ecdbfcc41dee6c05824478996f74b136863e6dc6
qianyu <443033099@qq.com>
2018-09-12 21:31:50 +0800

    Node 练习

---
+++ 20180911 nodejs/04-req.js
@@ -0,0 +1,8 @@
+const http = require('http')
+
+const server = http.createServer(function(req, res) {
+  console.log(`服务器接受到了请求 ${req}`)
+  res.end(`<h1>123</h1>`)
+})
+
+server.listen(3002, 'localhost')
--- 20180911 nodejs/05-url.js
+++ 20180911 nodejs/05-url.js
@@ -2,12 +2,7 @@ const http = require('http')
 
 const server = http.createServer(function(req, res) {
   console.log(`服务器接受到了请求 ${req.url}`)
-  res.writeHead(200, {
-    'Content-Type': 'text/html;charset=utf-8'
-  })
-  res.write('123123')
-  res.write('12312312da')
-  res.end('<h1>呵呵呵</h1>')
+  res.end(`<h1>123</h1>`)
 })
 
 server.listen(3002, 'localhost')
