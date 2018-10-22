e40d26b3bccdeb1b443ece18f4d1f7d417b116ad
SHERlocked93 <443033099@qq.com>
2018-10-22 09:14:33 +0800

    promise test1

--- 20181021 promise/promise test1.js
+++ 20181021 promise/promise test1.js
@@ -1,3 +1,11 @@
+/**
+ * 创建于 2018/10/22
+ * 作者: QianYu
+ * 功能: Promise 使用示例1
+ */
+
+
+
 function request() {
   return new Promise((resolve, reject) => {
     setTimeout(() => { resolve("success啦") }, 1000)
