a38fe2603e278891aaa0a6397267c1efbf79cc28
SHERlocked93 <443033099@qq.com>
2019-05-15 16:29:47 +0800

    更新本地文件打开方式

---
+++ 20190515_PLM/static-file-stream.js
@@ -0,0 +1,50 @@
+/**
+ * 创建于 2018/12/18
+ * 作者: SHERlocked93
+ * 功能: 静态资源 通过流的方式返回，前端blob接受
+ */
+
+const http = require('http')
+const path = require('path')
+const url = require('url')
+const fs = require('fs')
+let MIME_TYPE
+
+const resolve = dir => path.resolve(__dirname, dir)
+
+fs.readFile(resolve(`./mime.json`), (err, data) => {
+    if (err) throw Error('找不到mime.json')
+    MIME_TYPE = JSON.parse(data)
+})
+
+http
+    .createServer((req, res) => {
+        let { pathname } = url.parse(req.url)
+        if (pathname === '/') pathname = 'index.html'
+        if (!pathname.includes('.')) pathname = path.join(pathname, '/index.html')
+        
+        const extName = path.extname(pathname)
+        const fullPath = resolve(`../files/` + pathname)
+        
+        console.log(fullPath)
+        
+        fs.access(fullPath, function(err) {
+            if (err) res.end('notfount')
+            res.writeHead(200, {
+                'Content-Type': getMIME(extName),
+                'Access-Control-Allow-Origin': '*'
+                // 'Content-Disposition': 'attachment'   // 自动保存文件
+            })
+            fs.createReadStream(fullPath).pipe(res)
+        })
+    })
+    .listen(3002)
+
+/**
+ * 获取content-type
+ * @param extName
+ * @returns {string}
+ */
+function getMIME(extName) {
+    return MIME_TYPE[extName] || 'text/plain'
+}
