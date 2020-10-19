a247b1ce02fd22f4be1c9d6d58f2267ff6e0adc2
SHERlocked93 <443033099@qq.com>
2020-10-19 10:21:25 +0800

    feat: init react zhihu-ask

---
+++ 20201003_React/zhihu-ask/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Button } from 'antd-mobile'
+
+ReactDOM.render(
+  <Button>Start</Button>,
+  document.getElementById('root')
+)
